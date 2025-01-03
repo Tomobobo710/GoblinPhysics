d126920d3c694b62405f90aefbe25a3d0d03b9e0
Tomobobo710 <64335998+Tomobobo710@users.noreply.github.com>
2025-01-02 18:21:12 -0700

    Updated to Gulp 4 and fixed build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,45 @@
-var gulp = require( 'gulp' ),
-	uglify = require('gulp-uglify' ),
-	concat = require( 'gulp-concat' ),
-	jshint = require( 'gulp-jshint' );
+const { src, dest, series, parallel } = require('gulp');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const jshint = require('gulp-jshint');
 
-gulp.task('lint', function(){
-	gulp.src([
-		'src/classes/**/*.js'
-	])
-	.pipe( jshint() )
-	.pipe( jshint.reporter( 'default' ) );
-});
+function lint() {
+    return src('src/classes/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+}
 
-gulp.task('build', ['lint'], function(){
-	gulp.src([
-		'src/intro.js',
-		'src/classes/Math/**.js',
-		'src/libglobals.js',
-		'src/classes/EventEmitter.js',
-		'src/classes/RigidBody.js',
-		'src/classes/ForceGenerator.js',
-		'src/classes/**/*.js',
-		'src/outro.js'
-	])
-	.pipe( concat( 'goblin.js' ) )
-	.pipe( gulp.dest( 'build' ) );
-});
+const buildOrder = [
+    'src/intro.js',
+    'src/classes/Math/**.js',
+    'src/libglobals.js',
+    'src/classes/EventEmitter.js',
+    'src/classes/RigidBody.js',
+    'src/classes/ForceGenerator.js',
+    'src/classes/BroadPhases/BasicBroadphase.js',
+    'src/classes/BroadPhases/SAPBroadphase.js',
+    'src/classes/BroadPhases/**/*.js',
+    'src/classes/Collision/**/*.js',
+    'src/classes/Constraints/**/*.js',
+    'src/classes/ForceGenerators/**/*.js',
+    'src/classes/RayTracing/**/*.js',
+    'src/classes/Shapes/**/*.js',
+    'src/classes/Utils/**/*.js',
+    'src/classes/*.js',
+    'src/outro.js'
+];
 
-gulp.task('build-minified', ['lint'], function(){
-	gulp.src([
-		'src/intro.js',
-		'src/classes/Math/**.js',
-		'src/libglobals.js',
-		'src/classes/EventEmitter.js',
-		'src/classes/RigidBody.js',
-		'src/classes/ForceGenerator.js',
-		'src/classes/**/*.js',
-		'src/outro.js'
-	])
-	.pipe( concat( 'goblin.min.js' ) )
-	.pipe( uglify() )
-	.pipe( gulp.dest( 'build' ) );
-});
+function build() {
+    return src(buildOrder)
+        .pipe(concat('goblin.js'))
+        .pipe(dest('build'));
+}
 
-gulp.task('default', ['build', 'build-minified'], function(){});
+function buildMinified() {
+    return src(buildOrder)
+        .pipe(concat('goblin.min.js'))
+        .pipe(uglify())
+        .pipe(dest('build'));
+}
+
+exports.default = series(lint, parallel(build, buildMinified));
